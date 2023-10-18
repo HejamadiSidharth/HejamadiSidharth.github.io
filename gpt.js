db7702b2284987f5f750b4255936341aa7594a14
HejamadiSidharth <143036541+HejamadiSidharth@users.noreply.github.com>
2023-10-18 05:19:32 +0000

    hi

--- gpt.js
+++ gpt.js
@@ -1,34 +1,35 @@
-// Replace 'YOUR_API_KEY' with the API key you obtained.
-const apiKey = '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';
-const chatForm = document.getElementById('chat-form');
-const userInput = document.getElementById('user-input');
-const chatContainer = document.getElementById('chat-container');
+document.addEventListener("DOMContentLoaded", function() {
+    const chatForm = document.getElementById("chat-form");
+    const userInput = document.getElementById("user-input");
+    const chatContainer = document.getElementById("chat-container");
 
-chatForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const userMessage = userInput.value;
+    chatForm.addEventListener("submit", function(e) {
+        e.preventDefault();
+        const userMessage = userInput.value;
 
-    // Send user message to the server and display it in the chat.
-    chatContainer.innerHTML += `<div class="user-message">${userMessage}</div>`;
+        // Display user message
+        const userMessageElement = document.createElement("div");
+        userMessageElement.className = "chat-message user-message";
+        userMessageElement.textContent = userMessage;
+        chatContainer.appendChild(userMessageElement);
 
-    // Make a call to the OpenAI API.
-    fetch('https://api.openai.com/v1/engines/gpt-3.5-turbo/completions', {
-        method: 'POST',
-        headers: {
-            'Authorization': `Bearer ${apiKey}`,
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
+        // Call the OpenAI API
+        openai.completions.create({
+            engine: "davinci",
             prompt: userMessage,
-            max_tokens: 150, // Adjust as needed.
-        }),
-    })
-    .then(response => response.json())
-    .then(data => {
-        const aiMessage = data.choices[0].text;
-        chatContainer.innerHTML += `<div class="ai-message">${aiMessage}</div>`;
-    })
-    .catch(error => console.error('API Error:', error));
+            max_tokens: 150
+        })
+        .then(response => {
+            const aiMessage = response.choices[0].message.content;
 
-    userInput.value = '';
+            // Display AI message
+            const aiMessageElement = document.createElement("div");
+            aiMessageElement.className = "chat-message ai-message";
+            aiMessageElement.textContent = aiMessage;
+            chatContainer.appendChild(aiMessageElement);
+        })
+        .catch(error => console.error("API Error:", error));
+
+        userInput.value = "";
+    });
 });
